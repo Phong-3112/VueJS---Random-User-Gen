584c5b53e11b9663ab3b5ee1671c4b3540bf1c90
NamPhong <lyphong@mail.sorimachi.co.jp>
2021-09-30 22:41:29 +0900

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-          firstName: 'John',
+          firstName: 'Johnx',
           lastName: 'Doe',
           email: 'john@gmail.com',
           gender: 'male',
